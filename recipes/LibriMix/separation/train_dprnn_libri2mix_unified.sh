85d8c690297c129b5bbbbf5fe9877c6c8b1f8d66
YUCHEN005 <YUCHEN005@e.ntu.edu.sg>
2022-10-20 13:00:31 +0800

    upload code

--- recipes/LibriMix/separation/train_dprnn_libri2mix_unified.sh
+++ recipes/LibriMix/separation/train_dprnn_libri2mix_unified.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-#cmd="/path/to/slurm.pl --quiet"
-#
-#source activate CONDA_ENV
-#
-#$cmd log/dprnn-libri2mix-unified.log \
-#python train_unified.py hparams/dprnn-libri2mix-unified.yaml --data_folder /path/to/data/LibriMix/Libri2Mix/ --dynamic_mixing False
+cmd="/path/to/slurm.pl --quiet"
 
-
-
-cmd="/home3/chenchen/research/maison2/egs/VB/slurm.pl --quiet --nodelist=node01 --gpu 1 --num-threads 2"
-
-source activate asteroid
+source activate CONDA_ENV
 
 $cmd log/dprnn-libri2mix-unified.log \
-python train_unified.py hparams/dprnn-libri2mix-unified.yaml --data_folder /home3/chenchen/data/LibriMix/Libri2Mix/ --dynamic_mixing False
+python train_unified.py hparams/dprnn-libri2mix-unified.yaml --data_folder /path/to/data/LibriMix/Libri2Mix/ --dynamic_mixing False
+
