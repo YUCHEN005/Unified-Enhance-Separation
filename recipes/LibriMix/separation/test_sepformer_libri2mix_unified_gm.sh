d44fe0cb3abb62b12d8b76e04a34c87ab57024ec
YUCHEN005 <YUCHEN005@e.ntu.edu.sg>
2023-05-23 20:28:20 +0800

    Update code

--- recipes/LibriMix/separation/test_sepformer_libri2mix_unified_gm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cmd="/path/to/slurm.pl --quiet"
-
-source activate CONDA_ENV
-
-$cmd log/test-sepformer-libri2mix-unified-gm.log \
-python train.py hparams/sepformer-libri2mix-unified-gm.yaml --data_folder /path/to/data/LibriMix/Libri2Mix/ --test_only True
-
