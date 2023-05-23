3a832886ecb1f1f2a15f29fa1dc34b2906dec283
YUCHEN005 <YUCHEN005@e.ntu.edu.sg>
2023-05-23 21:13:17 +0800

    Update code

---
+++ tests/unittests/test_multi_mic.py
@@ -0,0 +1,33 @@
+import torch
+
+
+def test_gccphat():
+
+    from speechbrain.processing.features import STFT
+    from speechbrain.processing.multi_mic import Covariance, GccPhat
+
+    # Creating the test signal
+    fs = 16000
+
+    delay = 60
+
+    sig = torch.randn([10, fs])
+    sig_delayed = torch.cat((torch.zeros([10, delay]), sig[:, 0:-delay]), 1)
+
+    xs = torch.stack((sig_delayed, sig), -1)
+
+    stft = STFT(sample_rate=fs)
+    Xs = stft(xs)
+
+    # Computing the covariance matrix for GCC-PHAT
+    cov = Covariance()
+    gccphat = GccPhat()
+
+    XXs = cov(Xs)
+    tdoas = torch.abs(gccphat(XXs))
+
+    n_valid_tdoas = torch.sum(torch.abs(tdoas[..., 1] - delay) < 1e-3)
+    assert n_valid_tdoas == Xs.shape[0] * Xs.shape[1]
+    assert torch.jit.trace(stft, xs)
+    assert torch.jit.trace(cov, Xs)
+    assert torch.jit.trace(gccphat, XXs)
